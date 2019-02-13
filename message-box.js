f07c862261794871c6d9426c86f5513c6c066bcd
Tyler Ferrara <tylerferrara123@gmail.com>
2019-02-13 10:10:31 -0500

    Setup messaging structure

--- message-box.js
+++ message-box.js
@@ -3,15 +3,33 @@ const messageBox = document.getElementById('message-box');
 
 console.log(messageBox)
 
-function addMessage(msg) {
+function addMessage(msg, position) {
     let message = document.createElement('div');
     let container = document.createElement('div');
+    if (position === 'left') {
+        message.classList.add('message-left')
+    } else {
+        message.classList.add('message-right')
+    }
     message.innerHTML += msg;
-    message.className = 'message';
+    message.classList.add('message');
     container.className = 'message-container';
     container.appendChild(message);
     messageBox.appendChild(container);
     console.log('it was called');
 }
 
-addMessage('super-duper')
+addMessage('super-duper', 'left')
+addMessage('megladon', 'right')
+addMessage('super-duper', 'left')
+addMessage('megladon', 'right')
+addMessage('super-duper', 'left')
+addMessage('megladon', 'right')
+addMessage('super-duper', 'left')
+addMessage('megladon', 'right')
+addMessage('super-duper', 'left')
+addMessage('megladon', 'right')
+addMessage('super-duper', 'left')
+addMessage('megladon', 'right')
+addMessage('super-duper', 'left')
+addMessage('megladon', 'right')
